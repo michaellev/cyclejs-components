3081d92682dc020b1e27c4874a55b2409ab8e50e
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-03-15 13:26:20 +0200

    do not increment component version when repo unclean

--- scripts/increment-component-version.js
+++ scripts/increment-component-version.js
@@ -1,8 +1,10 @@
+const pify = require('pify')
 const readPkg = require('read-pkg')
 const writeJsonFile = require('write-json-file')
 const semver = require('semver')
 const { componentsDir, componentsPkgFilename } = require('./constants')
 const { resolve } = require('path')
+const git = require('simple-git')()
 
 const [,, componentName, magnitude] = process.argv
 
@@ -18,10 +20,19 @@ if (magnitudes.includes(magnitude) === false) {
 
 const pkgPath = resolve(componentsDir, componentName, componentsPkgFilename)
 
-readPkg(pkgPath, { normalize: false })
-  .then((pkg) => (
-    Object.assign({}, pkg, { version: semver.inc(pkg.version, magnitude) })
-  ))
-  .then((pkg) => (
-    writeJsonFile(pkgPath, pkg, { indent: 2, sortKeys: true })
-  ))
+const isGitCleanP = pify(git.status.bind(git))()
+  .then(status => status.files.length === 0)
+
+const pkgP = readPkg(pkgPath, { normalize: false })
+
+Promise
+  .all([isGitCleanP, pkgP])
+  .then(([ isGitClean, pkg ]) => {
+    if (!isGitClean) {
+      console.error('ERROR: repository is not clean')
+      process.exitCode = 1
+      return
+    }
+    const newPkg = Object.assign({}, pkg, { version: semver.inc(pkg.version, magnitude) })
+    writeJsonFile(pkgPath, newPkg, { indent: 2, sortKeys: true })
+  })
