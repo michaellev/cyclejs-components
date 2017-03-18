ac09fd0815d3571f499685105dabdc3954ef1697
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-03-18 13:20:58 +0200

    constants.componentNamesP excludes non-directories

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,9 +1,14 @@
 const pify = require('pify')
 const { resolve } = require('path')
 const { readdir } = require('fs')
+const { dir: isDir } = require('path-type')
+const pFilter = require('p-filter')
 
 const componentsDir = resolve(__dirname, '..', 'lib')
 const componentNamesP = pify(readdir)(resolve(componentsDir))
+  .then(paths => (
+    pFilter(paths, path => isDir(resolve(componentsDir, path)))
+  ))
 const componentDirsP = componentNamesP
   .then((names) => (
     names
