bf03746f01d4603e12322656cd329a171ef895b8
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-03-19 09:06:47 +0200

    clarify the package.json tests

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,7 +3,6 @@ const { resolve } = require('path')
 const { readdir } = require('fs')
 const { dir: isDir } = require('path-type')
 const pFilter = require('p-filter')
-const readPkg = require('read-pkg')
 
 const componentsDir = resolve(__dirname, '..', 'lib')
 const componentNamesP = pify(readdir)(resolve(componentsDir))
@@ -15,14 +14,9 @@ const componentDirsP = componentNamesP
     names
       .map(name => resolve(componentsDir, name))
   ))
-const componentPkgsP = componentDirsP
-  .then((componentDirs) => (
-    Promise.all(componentDirs.map(readPkg))
-  ))
 
 module.exports = {
   componentsDir: resolve(__dirname, '..', 'lib'),
   componentNamesP,
-  componentDirsP,
-  componentPkgsP
+  componentDirsP
 }
