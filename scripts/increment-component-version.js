1418d62743c97eea86433f86647b3a5f5a7be14e
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-03-19 09:49:43 +0200

    fix increment-component-version script

--- scripts/increment-component-version.js
+++ scripts/increment-component-version.js
@@ -1,15 +1,15 @@
 const pify = require('pify')
 const readPkg = require('read-pkg')
-const writeJsonFile = require('write-json-file')
+const writePkg = require('write-pkg')
 const semver = require('semver')
-const { componentsDir, componentsPkgFilename } = require('./constants')
+const { componentsDir } = require('./constants')
 const { resolve } = require('path')
 const git = require('simple-git')()
 
-const [,, componentName, magnitude, skipCleanCheck] = process.argv
+const [,, componentId, magnitude, skipCleanCheck] = process.argv
 
-if (componentName === undefined || magnitude === undefined) {
-  throw new Error('must provide both component name and magnitude. For example, `$ <command> clickable minor`')
+if (componentId === undefined || magnitude === undefined) {
+  throw new Error('must provide component id and magnitude. For example, `$ <command> clickable minor`')
 }
 
 const magnitudes = ['major', 'minor', 'patch']
@@ -18,31 +18,31 @@ if (magnitudes.includes(magnitude) === false) {
   throw new Error(`\`magnitude\` must be one of ${magnitudes}. It was ${magnitude}`)
 }
 
-const pkgPath = resolve(componentsDir, componentName, componentsPkgFilename)
-
 const isGitCleanP = pify(git.status.bind(git))()
   .then(status => status.files.length === 0)
 
-const pkgP = readPkg(pkgPath, { normalize: false })
+const componentDirectory = resolve(componentsDir, componentId)
+
+const pkgP = readPkg(componentDirectory, { normalize: false })
 
 Promise
   .all([isGitCleanP, pkgP])
-  .then(([ isGitClean, pkg ]) => {
+  .then(([isGitClean, pkg]) => {
     if (!skipCleanCheck && !isGitClean) {
       return Promise.reject(Error('repository is not clean'))
     }
 
     const version = semver.inc(pkg.version, magnitude)
     const newPkg = Object.assign({}, pkg, { version })
-    const writtenP = writeJsonFile(pkgPath, newPkg, { indent: 2, sortKeys: true })
+    const writtenP = writePkg(componentDirectory, newPkg)
 
     return Promise
       .all([version, writtenP])
   })
   .then(([version]) => {
-    const commitMsg = `${componentName}-v${version}`
+    const commitMsg = `${componentId}-v${version}`
     git
-      .add(pkgPath)
+      .add(resolve(componentDirectory, 'package.json'))
       .commit(commitMsg)
       .addTag(commitMsg)
       .push()
